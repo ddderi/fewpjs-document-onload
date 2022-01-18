b66241054353ad4b132f68ab0d41102458564c6c
ddderi <maxime830@live.fr>
2022-01-18 23:42:00 +0100

    commit tree 452299162105667a3d65fefd3a4a139d6357be0e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+
+// const txt = document.getElementById('text')
+
+// txt.innerHTML = 'This is really cool!'
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('The DOM has loaded')
+    txt.innerHTML = 'This is really cool!'
+})
+
+const txt = document.getElementById('text')
+
+//txt.innerHTML = 'This is really cool!'
